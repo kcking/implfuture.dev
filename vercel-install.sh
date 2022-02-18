6693edcc38e74f72352917240dbdebd3505c9d3b
Kevin King <4kevinking@gmail.com>
2022-02-17 20:35:03 -0800

    install wget

--- vercel-install.sh
+++ vercel-install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -x
+set -e
+yum install wget
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y && export PATH="/vercel/.cargo/bin:$PATH" && wget -O wasm-pack.tar.gz "https://github.com/rustwasm/wasm-pack/releases/download/v0.10.2/wasm-pack-v0.10.2-x86_64-unknown-linux-musl.tar.gz" && tar xvf wasm-pack.tar.gz && ls && yarn install
