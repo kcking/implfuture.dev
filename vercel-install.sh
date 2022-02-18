2e3632256483b254d3166a9056ae85bd716677f0
Kevin King <4kevinking@gmail.com>
2022-02-17 20:43:15 -0800

    move wasm-pack into place

--- vercel-install.sh
+++ vercel-install.sh
@@ -6,5 +6,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 export PATH="/vercel/.cargo/bin:$PATH" 
 wget -O wasm-pack.tar.gz "https://github.com/rustwasm/wasm-pack/releases/download/v0.10.2/wasm-pack-v0.10.2-x86_64-unknown-linux-musl.tar.gz" 
 tar xvf wasm-pack.tar.gz --wildcards --no-anchored 'wasm-pack' --strip-components=1
+rm wasm-pack.tar.gz
+chmod +x wasm-pack
+sudo mv wasm-pack /usr/bin
 ls 
 yarn install
