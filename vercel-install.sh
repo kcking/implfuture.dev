eb77748273f729ed385be38096df8128f0f5de14
Kevin King <4kevinking@gmail.com>
2022-02-17 20:39:17 -0800

    strip component

--- vercel-install.sh
+++ vercel-install.sh
@@ -2,4 +2,9 @@
 set -x
 set -e
 yum install wget
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y && export PATH="/vercel/.cargo/bin:$PATH" && wget -O wasm-pack.tar.gz "https://github.com/rustwasm/wasm-pack/releases/download/v0.10.2/wasm-pack-v0.10.2-x86_64-unknown-linux-musl.tar.gz" && tar xvf wasm-pack.tar.gz --wildcards --no-anchored 'wasm-pack' && ls && yarn install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y 
+export PATH="/vercel/.cargo/bin:$PATH" 
+wget -O wasm-pack.tar.gz "https://github.com/rustwasm/wasm-pack/releases/download/v0.10.2/wasm-pack-v0.10.2-x86_64-unknown-linux-musl.tar.gz" 
+tar xvf wasm-pack.tar.gz --wildcards --no-anchored 'wasm-pack' --strip-components=1
+ls 
+yarn install
