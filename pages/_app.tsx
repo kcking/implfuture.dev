5ef2c5a2aa28a86fd41800bc0c58e944114647d2
Kevin King <4kevinking@gmail.com>
2022-02-18 18:10:18 -0800

    revert plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import Header from "../components/header";
 import styles from "../styles/Home.module.css";
 import "../styles/prism-vs.css";
 import "../styles/prism-vsc-dark-plus.css";
-import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -15,12 +14,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <PlausibleProvider domain={"implfuture.dev"}>
-        <main className={styles.main + " font-body"}>
-          <Header></Header>
-          <Component {...pageProps} />
-        </main>
-      </PlausibleProvider>
+      <main className={styles.main + " font-body"}>
+        <Header></Header>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
