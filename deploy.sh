2dbdfcd959d8b89073a9f9fe45b4f7904dbf0942
Kevin King <kcking@users.noreply.github.com>
2022-09-30 19:38:05 -0700

    use --stamp flag on deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set -ex
 
 export AWS_PAGER=""
 
-bazel run -c opt //server:push-amd64
+bazel run -c opt //server:push-amd64 --stamp
 aws lambda update-function-code --function-name=implfuture --image-uri=689191389309.dkr.ecr.us-west-1.amazonaws.com/implfuture@$(cat target/bin/server/push-amd64.digest)
