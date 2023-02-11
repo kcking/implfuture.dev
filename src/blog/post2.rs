1e50a3308d7245f3b56fba2a3bf0443f30ee8211
Kevin King <4kevinking@gmail.com>
2023-02-11 12:24:20 -0800

    Split blog post to workaround `locals exceed maximum`

--- src/blog/post2.rs
+++ src/blog/post2.rs
@@ -1,5 +1,18 @@
 use super::*;
 
 pub fn post_2(_: &Metadata) -> Html {
-    include_mdx!("src/blog/post2.mdx")
+    //  split post into multiple parts to workaround `locals exceed maximum` error
+    html! {
+        <>
+        {post_2_0()}
+        {post_2_1()}
+        </>
+    }
+}
+
+fn post_2_0() -> Html {
+    include_mdx!("src/blog/post2_0.mdx")
+}
+fn post_2_1() -> Html {
+    include_mdx!("src/blog/post2_1.mdx")
 }
