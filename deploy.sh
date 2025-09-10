6c202a58fc68e7bdd3222090afddc18c170b5cb1
Kevin King <4kevinking@gmail.com>
2025-09-10 01:05:44 -0700

    WIP amd64

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -ex
 
 export AWS_PAGER=""
 
-OUTPUT_PATH=$(bazel cquery -c opt //server:image_multiarch --output=starlark --starlark:expr='target.files.to_list()[0].path')
+OUTPUT_PATH=$(bazel cquery -c opt //server:image-amd64 --output=starlark --starlark:expr='target.files.to_list()[0].path')
 bazel run -c opt //server:push --stamp
 DIGEST=$(cat $OUTPUT_PATH/index.json | jq -r '.manifests[0].digest')
 aws lambda update-function-code --function-name=implfuture --image-uri="689191389309.dkr.ecr.us-west-1.amazonaws.com/implfuture@${DIGEST}"
